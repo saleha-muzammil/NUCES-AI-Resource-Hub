73ec6f40ba8d611e5cdf1b841cdee28c887a7808
Saleha <saleha@192.168.1.11>
2025-05-04 14:53:24 +0500

    fix errors

--- src/app/src/app/domains/page.tsx
+++
@@ -1,5 +0,0 @@
-<ul className="space-y-2 text-gray-300">
-  <li>• &quot;Deep Learning&quot; by Ian Goodfellow</li>
-  <li>• &quot;Pattern Recognition and Machine Learning&quot; by Christopher Bishop</li>
-  <li>• &quot;Artificial Intelligence: A Modern Approach&quot; by Stuart Russell</li>
-</ul> 
